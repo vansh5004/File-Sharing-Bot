2741a061f19587796684caeddb736ae2c66e8504
Vansh Kumar <145826508+vansh5004@users.noreply.github.com>
2023-11-11 21:30:32 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/rudraraut'>Click here</a>\n○ Channel : @rudraraut\n○ Support Group : https://t.me/+G3e6AHAPzUEzNDM1</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/moviekorner_1'>Click here</a>\nAdmin : <a href='https://t.me/vip_bro10'>Admin</a>\nGroup : <a href='https://t.me/moviekorner_1'>Group</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
